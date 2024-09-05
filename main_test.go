17422b051fedf762c616e528a8fd148e53f94966
Maxim Buluchevskiy <maxim.buluchevskiy@mechta.kz>
2024-09-05 09:47:24 +0500

    second iteration

--- main_test.go
+++ main_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
@@ -14,33 +15,23 @@ func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 
 	// Запрос с параметрами count, превышающим общее количество кафе, и city = "moscow"
 	req, err := http.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
-	if err != nil {
-		t.Fatalf("Не удалось создать запрос: %v", err)
-	}
+	require.NoError(t, err)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	if status := responseRecorder.Code; status != http.StatusOK {
-		t.Errorf("Ожидался статус-код %v, но получен %v", http.StatusOK, status)
-	}
+	require.Equal(t, http.StatusOK, responseRecorder.Code)
 
 	expectedResponse := "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент"
-	responseBody := responseRecorder.Body.String()
-
-	assert.Equal(t, expectedResponse, responseBody, "Ответ не совпадает с ожидаемым")
-
-	assert.NotEmpty(t, responseBody, "Ответ не должен быть пустым")
-	assert.Len(t, strings.Split(responseBody, ","), totalCount, "Количество кафе в ответе должно быть равно totalCount")
+	assert.Equal(t, expectedResponse, responseRecorder.Body.String())
+	assert.Len(t, strings.Split(responseRecorder.Body.String(), ","), totalCount)
 }
 
 func TestMainHandlerWhenCityNotSupported(t *testing.T) {
 	// Запрос с некорректным городом
 	req, err := http.NewRequest("GET", "/cafe?count=2&city=unknown", nil)
-	if err != nil {
-		t.Fatalf("Не удалось создать запрос: %v", err)
-	}
+	require.NoError(t, err)
 
 	responseRecorder := httptest.NewRecorder()
 
@@ -48,20 +39,16 @@ func TestMainHandlerWhenCityNotSupported(t *testing.T) {
 
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code, "Ожидался статус-код 400")
+	assert.Equal(t, http.StatusBadRequest, responseRecorder.Code)
 
 	expectedResponse := "wrong city value"
-	responseBody := responseRecorder.Body.String()
-
-	assert.Equal(t, expectedResponse, responseBody, "Ответ не совпадает с ожидаемым")
+	assert.Equal(t, expectedResponse, responseRecorder.Body.String())
 }
 
 func TestMainHandlerWhenCorrectRequest(t *testing.T) {
 	// Запрос с корректными параметрами
 	req, err := http.NewRequest("GET", "/cafe?count=2&city=moscow", nil)
-	if err != nil {
-		t.Fatalf("Не удалось создать запрос: %v", err)
-	}
+	require.NoError(t, err)
 
 	responseRecorder := httptest.NewRecorder()
 
@@ -69,11 +56,8 @@ func TestMainHandlerWhenCorrectRequest(t *testing.T) {
 
 	handler.ServeHTTP(responseRecorder, req)
 
-	assert.Equal(t, http.StatusOK, responseRecorder.Code, "Ожидался статус-код 200")
+	assert.Equal(t, http.StatusOK, responseRecorder.Code)
 
 	expectedResponse := "Мир кофе,Сладкоежка"
-	responseBody := responseRecorder.Body.String()
-
-	assert.Equal(t, expectedResponse, responseBody, "Ответ не совпадает с ожидаемым")
-	assert.NotEmpty(t, responseBody, "Ответ не должен быть пустым")
+	assert.Equal(t, expectedResponse, responseRecorder.Body.String())
 }
